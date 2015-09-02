bb421cda6a4b13d338cf33de6176b2e3e03db865
Tomas Caballero <tomycaballero95@gmail.com>
2015-09-01 21:33:21 -0300

    Últimos ejercicios

--- clase3.hs
+++ clase3.hs
@@ -1,3 +1,4 @@
+import Data.Char
 -- CREAR PAR --
 crearPar :: a -> b -> (a, b)
 crearPar x y = (x, y)
@@ -16,3 +17,9 @@ listar x y z = [x, y, z]
 -- RANGO DE PASO --
 rangoDePaso :: Integer -> Integer -> Integer -> [Integer]
 rangoDePaso n1 n2 n3 = [n1, n1 + n3 ..n2]
+-- PENDIENTE --
+pendiente :: (Float, Float) -> (Float, Float) -> Float
+pendiente p1 p2 = (snd p1- snd p2)/(fst p1 - fst p2)
+-- INICIALES --
+iniciales :: String -> String -> String
+iniciales nom ape = (head nom : ".") ++ (head ape : ".")  
