87043603daff5eef07ccdf7d1042f4cfd7192ff6
Tomas Caballero <tomycaballero95@gmail.com>
2015-11-05 23:40:37 -0300

    Actualizacion ejercicios

--- clase10.hs
+++ clase10.hs
@@ -2,32 +2,48 @@ data Polinomio = Mono Float Integer |
 				 Suma Polinomio Polinomio |
 				 Producto Polinomio Polinomio
 
-evaluar :: Polinomio -> Float -> Float
-evaluar (Mono coef grado) x = coef * (x^grado)
-evaluar (Suma p1 p2) x = (evaluar p1 x) + (evaluar p2 x)
-evaluar (Producto p1 p2) x = (evaluar p1 x) * (evaluar p2 x)
+sumaListas :: Num a => [a] -> [a] -> [a]
+sumaListas [] l = l
+sumaListas l [] = l
+sumaListas (x:xs) (y:ys) = (x + y): (sumaListas xs ys)
 
-maxGrado :: Polinomio -> Integer
-maxGrado (Mono coef grado) = grado
-maxGrado (Suma p1 p2) = max (maxGrado p1) (maxGrado p2)
+porListas :: Num a => [a] -> [a] -> [a]
+porListas [] l = []
+porListas l [] = []
+porListas [x] (l:ls) = (x*l):(porListas [x] ls)
+porListas (x:xs) l = sumaListas (porListas [x] l) (0:(porListas xs l))
+
+evaluar :: Polinomio -> Float -> Float
+evaluar (Mono a n) z = a*(z^n)
+evaluar (Suma p q) z = (evaluar p z) + (evaluar q z)
+evaluar (Producto p q) z = (evaluar p z) * (evaluar q z)
 
 coeficientes :: Polinomio -> [Float]
-coeficientes (Mono 0 _) = []
-coeficientes (Mono coef grado) = [coef]
-coeficientes (Suma p1 p2) 
-		| maxGrado p1 == ((maxGrado p2) + 1) = coeficientes p2 ++ coeficientes p1
-		| otherwise = coeficientes p2 ++ [0.0] ++ coeficientes p1 
+coeficientes p = limpiarCeros (coeficientes' p)
 
-x :: Polinomio
-x = Mono 1 1
+coeficientes' (Mono a 0) = [a]
+coeficientes' (Mono a n) = 0:(coeficientes' (Mono a (n-1)))
+coeficientes' (Suma p q) = sumaListas (coeficientes' p) (coeficientes' q)
+coeficientes' (Producto p q) = porListas (coeficientes' p) (coeficientes' q)
+
+sacarCeros :: [Float] -> [Float]
+sacarCeros (0:xs) = sacarCeros xs
+sacarCeros l = l
+
+limpiarCeros :: [Float] -> [Float]
+limpiarCeros l = reverse (sacarCeros (reverse l))
 
 instance Num Polinomio where
 		(+) p q = Suma p q
-		(*) p q = Producto p q 
+		(*) p q = Producto p q
 		negate p = Producto p (Mono (-1) 0)
 		fromInteger n = Mono 0 0
 		abs p = undefined
 		signum p = undefined
+
+instance Show Polinomio where
+	show (Mono a n) = show a ++ "x^" ++ show n
+	show (Suma p q) = show p ++ " + " ++ show q
 --data Polinomio a = Mono a Integer |
 --				   Suma (Polinomio a) (Polinomio a) |
 --				   Producto (Polinomio a) (Polinomio a)
